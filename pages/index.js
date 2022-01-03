fcda52308584e6f8a2c1ad89af767cbc269b6965
Sushil <sushilmangal731@gmail.com>
2022-01-03 14:46:10 +0530

    calander and search

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,28 @@
-import Head from 'next/head';
+import Head from "next/head";
 
-import Banner from '../components/Banner';
-import Header from '../components/Header';
-import SmallCard from '../components/SmallCard';
-import MediumCard from '../components/MediumCard';
-import LargeCard from '../components/LargeCard';
-import Footer from '../components/Footer';
+import Banner from "../components/Banner";
+import Header from "../components/Header";
+import SmallCard from "../components/SmallCard";
+import MediumCard from "../components/MediumCard";
+import LargeCard from "../components/LargeCard";
+import Footer from "../components/Footer";
 
 export default function Home({ exploreData, cardsData }) {
   return (
-    <div className=''>
+    <div className="">
       <Head>
         <title>Booking App</title>
-        <link rel='icon' href='/favicon.ico' />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header />
       <Banner />
 
-      <main className='max-w-7xl mx-auto px-8 sm:px-16'>
-        <section className='pt-5'>
-          <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-5">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
 
-          <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
             {exploreData?.map(({ img, distance, location }) => (
               <h1>
                 <SmallCard
@@ -37,9 +37,9 @@ export default function Home({ exploreData, cardsData }) {
         </section>
 
         <section>
-          <h2 className='text-4xl font-semibold py-8'>Live Anywhere</h2>
+          <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
 
-          <div className='flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3'>
+          <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
             {cardsData?.map(({ img, title }) => (
               <MediumCard key={img} img={img} title={title} />
             ))}
@@ -47,10 +47,10 @@ export default function Home({ exploreData, cardsData }) {
         </section>
 
         <LargeCard
-          img='https://links.papareact.com/4cj'
-          title='The greatest Outdoors'
-          description='Wishlists curated by Airbnb.'
-          buttonText='Get Inspired'
+          img="https://links.papareact.com/4cj"
+          title="The greatest Outdoors"
+          description="Wishlists curated by Airbnb."
+          buttonText="Get Inspired"
         />
       </main>
 
@@ -60,11 +60,11 @@ export default function Home({ exploreData, cardsData }) {
 }
 
 export async function getStaticProps() {
-  const exploreData = await fetch('https://links.papareact.com/pyp').then(
+  const exploreData = await fetch("https://links.papareact.com/pyp").then(
     (response) => response.json()
   );
 
-  const cardsData = await fetch('https://links.papareact.com/zp1').then(
+  const cardsData = await fetch("https://links.papareact.com/zp1").then(
     (response) => response.json()
   );
 
